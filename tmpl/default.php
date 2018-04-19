3a1baf1b1c603ae782ffbafadb5022724ccccb71
Iosif Chatzimichail <info@reach.gr>
2018-04-19 20:01:43 +0300

    Add jQuery noconflict for *those* sites

--- tmpl/default.php
+++ tmpl/default.php
@@ -47,39 +47,42 @@ $image = json_decode($banner->params);
 	}
 </style>
 <script>
-$(document).ready(function() {
-	if ($('.rpopup-container').length > 0) {
-		$('.rpopup-container').each(function(){
-			var id = $(this).attr('id');
-			if (document.cookie.split(';').indexOf(id+'=true') < 0) {
-    			setTimeout(function(){$('#'+id).addClass('open');}, 1500);
-			}
-		});		
-	}
-	function setCookie(id) {
-		var date = new Date();
-		date.setTime(date.getTime()+(3*24*60*60*1000));
-		document.cookie = 'rpopup-'+id+'=true;expires='+date.toGMTString()+';path=/';
-	}
-	$('.rpopup-close-button').click(function(event) {
-		var id = $(this).data('close');	
-		<?php if ($params->get('gevent') == '1'): ?>
-		ga('send','event', {'eventCategory': 'Popup','eventAction': 'Close'});
-		<?php endif; ?>
-		setCookie(id);
-		$('#rpopup-'+id).removeClass('open');
+jQuery.noConflict();
+(function($) {
+	$(document).ready(function() {
+		if ($('.rpopup-container').length > 0) {
+			$('.rpopup-container').each(function(){
+				var id = $(this).attr('id');
+				if (document.cookie.split(';').indexOf(id+'=true') < 0) {
+	    			setTimeout(function(){$('#'+id).addClass('open');}, 1500);
+				}
+			});		
+		}
+		function setCookie(id) {
+			var date = new Date();
+			date.setTime(date.getTime()+(3*24*60*60*1000));
+			document.cookie = 'rpopup-'+id+'=true;expires='+date.toGMTString()+';path=/';
+		}
+		$('.rpopup-close-button').click(function(event) {
+			var id = $(this).data('close');	
+			<?php if ($params->get('gevent') == '1'): ?>
+			ga('send','event', {'eventCategory': 'Popup','eventAction': 'Close'});
+			<?php endif; ?>
+			setCookie(id);
+			$('#rpopup-'+id).removeClass('open');
 
+		});
+		$('a.rpopup-link').click(function(event) {
+			var id = $(this).data('close');
+			var href = $(this).href;
+			<?php if ($params->get('gevent') == '1'): ?>
+			ga('send','event', {'eventCategory': 'Popup','eventAction': 'Click'});
+			<?php endif; ?>
+			setCookie(id);
+			$('#rpopup-'+id).removeClass('open');
+			window.location = href;
+		});
 	});
-	$('a.rpopup-link').click(function(event) {
-		var id = $(this).data('close');
-		var href = $(this).href;
-		<?php if ($params->get('gevent') == '1'): ?>
-		ga('send','event', {'eventCategory': 'Popup','eventAction': 'Click'});
-		<?php endif; ?>
-		setCookie(id);
-		$('#rpopup-'+id).removeClass('open');
-		window.location = href;
-	});
-});
+})(jQuery);
 </script>
 <?php endif; ?>
